fd6ad9fcde0d442b93fbfe93e978a86b4a931973
Robin Thellend <rthellend@gmail.com>
2024-11-20 19:46:50 -0800

    Add build workflow (#2)

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd $(dirname $0)
 echo "Updating xtermjs..."
 ./xterm/update.sh
 echo "Updating ssh.wasm..."
 ./go/build.sh
+echo "Files in ./docroot/"
+ls -l ./docroot/
 echo "Done"
