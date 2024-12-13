ccdeb037f74bfaa9ba4c391217c28e091a1290cf
Robin Thellend <rthellend@gmail.com>
2024-12-13 11:32:36 -0800

    Minor change for go1.24

--- go/build.sh
+++ go/build.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 GOOS=js GOARCH=wasm go build -ldflags="-extldflags=-s -w" -o ../docroot/ssh.wasm .
 GOOS=js GOARCH=wasm go test -c -o ../docroot/tests.wasm ./internal/tests
-cp $(go env GOROOT)/misc/wasm/wasm_exec.js ../docroot/
+cp $(go env GOROOT)/*/wasm/wasm_exec.js ../docroot/
