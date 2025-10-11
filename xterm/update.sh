b8bd7d0f94e6745d034c0a5377bb6d210ce94d79
Robin Thellend <rthellend@gmail.com>
2025-10-11 07:40:50 -0700

    npx

--- xterm/update.sh
+++ xterm/update.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 
 npm install
 
-./node_modules/browserify/bin/cmd.js browser.js -o ../docroot/xterm.js
+npx browserify browser.js -o ../docroot/xterm.js
 cp node_modules/@xterm/xterm/css/xterm.css ../docroot/xterm.css
 cp node_modules/@xterm/xterm/LICENSE ../docroot/LICENSE.xterm.txt
