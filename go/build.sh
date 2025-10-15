88740821a6dd265055e9dccf2c9fbeffd2901a14
Robin Thellend <rthellend@gmail.com>
2025-10-15 10:27:10 -0700

    Use cp -f for wasm_exec.js

--- go/build.sh
+++ go/build.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 GOOS=js GOARCH=wasm go build -ldflags="-extldflags=-s -w" -o ../docroot/ssh.wasm .
 GOOS=js GOARCH=wasm go test -c -o ../docroot/tests.wasm ./internal/tests
-cp $(go env GOROOT)/lib/wasm/wasm_exec.js ../docroot/
+cp -f $(go env GOROOT)/lib/wasm/wasm_exec.js ../docroot/
